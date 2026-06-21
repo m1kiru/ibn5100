6a751133dc3dff428f2743f127b7aa66b5bdd4b5
makiru <136336292+m1kiru@users.noreply.github.com>
2026-06-21 23:41:36 +0500

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
 
 {
@@ -9,11 +5,20 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  # NixEspecially
+  # GRUB
+  boot.loader = {
+    grub = {
+      enable = true;
+      device = "nodev"; # "nodev" is used for UEFI
+      efiSupport = true;
+    };
+    efi.canTouchEfiVariables = true;
+  };
+  # Flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # Allow unfree pkgs
+  nixpkgs.config.allowUnfree = true;
+  # Mount ntfs drives
   fileSystems =
     let
       ntfs-drives = [
@@ -27,16 +32,21 @@
         "uid=1000"
         "nofail"
       ];
-    }); 
-  # IOS
-  services.usbmuxd.enable = true;
-  # Use latest kernel.
+    });
+  # Kernel
   boot.kernelPackages = pkgs.linuxPackages_zen;
-  # Dynamically linked executables
-  networking.hostName = "ibn5100"; # Define your hostname.
-
+  # Define your hostname.
+  networking.hostName = "ibn5100";
+  # niri
+  programs.niri.enable = true;
+  #waybar
+  programs.waybar.enable = true;
+  # Ly login mnger
+  services.displayManager.ly.enable = true;
   # Configure network connections interactively with nmcli or nmtui.
   networking.networkmanager.enable = true;
+  # vpn
+  #services.v2raya.enable = true;
 
   # Set your time zone.
   time.timeZone = "Asia/Yekaterinburg";
@@ -51,8 +61,9 @@
   i18n.extraLocaleSettings = {
     LC_ALL = "ru_RU.UTF-8";
   };
-  i18n.extraLocales = ["ja_JP.UTF-8/UTF-8"];
-  
+  # Extra locales to use
+  #i18n.extraLocales = ["ja_JP.UTF-8/UTF-8"];
+
   console = {
     font = "Lat2-Terminus16";
     useXkbConfig = true; # use xkb.options in tty.
@@ -61,16 +72,16 @@
   # X11 & nvidia sect
   hardware.graphics.enable = true;
   hardware.graphics.enable32Bit = true;
-  services.xserver = { 
+  services.xserver = {
     enable = true;
     videoDrivers = [ "nvidia" ];
   };
   hardware.nvidia = {
     open = false;
-    modesetting.enable = true; 
+    modesetting.enable = true;
   };
   services.lact.enable = true;
-  # Gaming sect
+  # Steam
   programs.steam = {
   enable = true;
   dedicatedServer.openFirewall = true;
@@ -79,14 +90,7 @@
     "steam"
     "steam-unwrapped"
   ];
-  # Gnome sect
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
-  services.gnome.core-apps.enable = false;
-  services.gnome.core-developer-tools.enable = false; 
-  services.gnome.games.enable = false;
-  environment.gnome.excludePackages = with pkgs; [ gnome-tour gnome-user-docs];
-  
+
   # Configure keymap in X11
   services.xserver.xkb.layout = "us,ru";
   services.xserver.xkb.options = "grp:win_space_toogle";
@@ -95,7 +99,7 @@
   # services.printing.enable = true;
 
   # Enable sound.
-  security.rtkit.enable = true; 
+  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
@@ -105,11 +109,6 @@
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.libinput.enable = true;
-  
-  # NixSpecialSettings sect
-  nixpkgs.config.allowUnfree = true;
-
-
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.makiru = {
@@ -117,50 +116,34 @@
     extraGroups = [ "wheel" "networkmanager"]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [ ];
   };
-  # programs.firefox.enable = true;
-  # Hamachi
-  services.zerotierone = {
-    enable = true;
-  };
 
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
-  
+
   environment.systemPackages = with pkgs; [
     wget
-    google-chrome
-    gnome-tweaks
     alacritty
-    nautilus
-    clementine 
-    libimobiledevice
-    ifuse
-    gnome-text-editor
+    clementine
     git
-    protonup-qt
-    fastfetch
     nftables
-    lact
+    lacts
     btop
     gamemode
-    appeditor
+    gamescope
     vlc
-    # Unstable
-    discord
-    android-studio
     ayugram-desktop
-    amnezia-vpn
-    vscodium
-    code-cursor
-    v2rayn
-    lmstudio
     qbittorrent
+    ibm-plex
+    qt5ct
+    librewolf
+    cloudflare-warp
   ];
 
+  # /environment
   environment.variables = {
     "__GL_SHADER_DISK_CACHE_SIZE" = "12000000000";
-    ELECTRON_OZONE_PLATFORM_HINT = "auto";
-    QT_QPA_PLATFORMTHEME = "qt5ct";
+    "ELECTRON_OZONE_PLATFORM_HINT" = "auto";
+    "QT_QPA_PLATFORMTHEME" = "qt5ct";
   };
   # Strawberry fix
   environment.sessionVariables.XDG_DATA_DIRS = [ "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}" "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}" ];
@@ -169,33 +152,32 @@
   # V2ray
   #services.v2rayn.enable = true;
   #anidesk, hiddify
-  
-  programs.nix-ld = {
-    enable = true;
-    libraries = with pkgs; [
-      glib
-      nspr
-      nss
-      dbus
-      atk
-      cups
-      cairo
-      gtk3
-      pango
-      libx11
-      libxcomposite
-      libxdamage
-      libxext
-      libxfixes
-      libxrandr
-      libgbm
-      expat
-      libxcb
-      libxkbcommon
-      alsa-lib-with-plugins
-      libGL
-    ];
-  };
+  # programs.nix-ld = {
+  #   enable = true;
+  #   libraries = with pkgs; [
+  #     glib
+  #     nspr
+  #     nss
+  #     dbus
+  #     atk
+  #     cups
+  #     cairo
+  #     gtk3
+  #     pango
+  #     libx11
+  #     libxcomposite
+  #     libxdamage
+  #     libxext
+  #     libxfixes
+  #     libxrandr
+  #     libgbm
+  #     expat
+  #     libxcb
+  #     libxkbcommon
+  #     alsa-lib-with-plugins
+  #     libGL
+  #   ];
+  # };
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -204,7 +186,6 @@
   #   enableSSHSupport = true;
   # };
   # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
@@ -236,7 +217,6 @@
   # and migrated your data accordingly.
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "25.11"; # Did you read the comment?
+  system.stateVersion = "26.05"; # Did you read the comment?
 
 }
-
